b2f6ec0914123e6c9930fdb5ea45db6c0742d173
alev7ina <alev7ina@gmail.com>
2019-11-11 13:39:19 -0600

    Added RedirectingService and Controller

---
+++ Mlnky.Business/Services/Implementations/RedirectingService.cs
@@ -0,0 +1,19 @@
+﻿using Mlnky.Common.Repositories;
+using Mlnky.DataAccess.MangoDB.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mlnky.Business.Services.Implementations
+{
+    public class RedirectingService : IRedirectingService
+    {
+
+        public string GetLongUrl(string shortCode)
+        {
+            IShorteningsRepository myRepo = new ShorteningsRepository();
+            var result = myRepo.FindOne(shortCode);
+            return result.LongUrl;
+        }
+    }
+}
---
+++ Mlnky.RestApi/Controllers/UrlController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mlnky.Business.Services;
+using Mlnky.Business.Services.Implementations;
+
+namespace Mlnky.RestApi.Controllers
+{
+    [Route("api/Url")]
+    [ApiController]
+    public class UrlController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{url}")]
+        public ActionResult<string> Get(string url)
+        {
+            IRedirectingService myService = new RedirectingService();
+            var result = myService.GetLongUrl(url);
+            return result;
+        }
+    }
+}
