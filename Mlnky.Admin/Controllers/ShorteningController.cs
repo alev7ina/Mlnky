8b69d1fdb887b6a43338451ad253dbd0d8422dad
Alevtina Brown <40323674+alev7ina@users.noreply.github.com>
2019-11-20 13:30:25 -0600

    Added MVC to Admin UI

---
+++ Mlnky.Admin/Controllers/ShorteningController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mlnky.Admin.Models;
+
+namespace Mlnky.Admin.Controllers
+{
+    public class ShorteningController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Shorten(ShorteningRequestViewModel request)
+        {
+            var model = new ShorteningResponseViewModel();
+            model.ShortUrl = request.LongUrl;
+
+            return View(model);
+        }
+    }
+}
---
+++ Mlnky.Admin/Models/ShorteningRequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mlnky.Admin.Models
+{
+    public class ShorteningRequestViewModel
+    {
+        [Required(ErrorMessage = "Need to put in a value")]
+        [Display(Name = "URL to Shorten")]
+        public string LongUrl { get; set; }
+    }
+}
